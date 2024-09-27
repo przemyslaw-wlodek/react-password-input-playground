ee59a86c3760f42a9649b7ee265a708a792e0d21
Przemysław Włodek <przem.wlodek.github@gmail.com>
2024-09-27 12:50:53 +0200

    switch to react 17 (lace)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { render } from 'react-dom';
+import App from './App.tsx';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+  document.getElementById('root')!,
+);
